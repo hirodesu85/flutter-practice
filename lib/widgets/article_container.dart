f4c18537f2649217297c275407fa13aa6a22301e
OGATA Hiromu <hiromukkun@gmail.com>
2024-12-10 18:29:31 +0900

    add:実装完了

--- lib/widgets/article_container.dart
+++ lib/widgets/article_container.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_practice/models/article.dart';
 import 'package:intl/intl.dart';
+import 'package:flutter_practice/screens/article_screen.dart';
 
 class ArticleContainer extends StatelessWidget {
   const ArticleContainer({
@@ -17,91 +18,100 @@ class ArticleContainer extends StatelessWidget {
         vertical: 12,
         horizontal: 16,
       ),
-      child: Container(
-        height: 180,
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 16,
-        ),
-        decoration: const BoxDecoration(
-          color: Color(0xFF55C500),
-          borderRadius: BorderRadius.all(Radius.circular(32)),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // 投稿日
-            Text(
-              DateFormat('yyyy/MM/dd').format(article.createdAt),
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 12,
-              ),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => ArticleScreen(article: article),
             ),
-            // タイトル
-            Text(
-              article.title,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            // タグ
-            Text(
-              '#${article.tags.join(' #')}',
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(
-                color: Colors.white,
-                fontStyle: FontStyle.italic,
-              ),
-            ),
-            // いいね、投稿者
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                // いいね数
-                Column(
-                  children: [
-                    const Icon(
-                      Icons.favorite,
-                      color: Colors.white,
-                    ),
-                    Text(
-                      article.likesCount.toString(),
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 12,
-                      ),
-                    ),
-                  ],
+          );
+        },
+        child: Container(
+          height: 180,
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 16,
+          ),
+          decoration: const BoxDecoration(
+            color: Color(0xFF55C500),
+            borderRadius: BorderRadius.all(Radius.circular(32)),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // 投稿日
+              Text(
+                DateFormat('yyyy/MM/dd').format(article.createdAt),
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
                 ),
-                // 投稿者
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    CircleAvatar(
-                      radius: 26,
-                      backgroundImage:
-                          NetworkImage(article.user.profileImageUrl),
-                    ),
-                    const SizedBox(height: 4),
-                    Text(
-                      article.user.id,
-                      style: const TextStyle(
+              ),
+              // タイトル
+              Text(
+                article.title,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              // タグ
+              Text(
+                '#${article.tags.join(' #')}',
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+              // いいね、投稿者
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  // いいね数
+                  Column(
+                    children: [
+                      const Icon(
+                        Icons.favorite,
                         color: Colors.white,
-                        fontSize: 12,
                       ),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ],
+                      Text(
+                        article.likesCount.toString(),
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                  // 投稿者
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      CircleAvatar(
+                        radius: 26,
+                        backgroundImage:
+                            NetworkImage(article.user.profileImageUrl),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        article.user.id,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
